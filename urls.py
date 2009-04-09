1150a503ac9f1dc85415b6cec630824f7b9e4077
marga <marga@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-04-09 23:22:00 +0000

    Get the admin module to work in Django 1.0
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@60 19058424-5d2e-0410-ba3e-95a0a9172157

--- urls.py
+++ urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
 
+# The next two lines enable the admin and load each admin.py file:
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+
+	# The admin interface
+	(r'^admin/(.*)', admin.site.root),
 
 	# The amancay app
 	(r'^amancay/', include('bts_webui.amancay.urls')),
@@ -8,9 +15,6 @@ urlpatterns = patterns('',
 	# The registration app
 	(r'^accounts/profile/', include('bts_webui.amancay.urls')),
 	(r'^accounts/', include('bts_webui.registration.urls')),
-
-	# Uncomment this for admin:
-    (r'^admin/', include('django.contrib.admin.urls')),
 
 	# amancay is the main site here.
 	(r'^/?', include('bts_webui.amancay.urls')),
