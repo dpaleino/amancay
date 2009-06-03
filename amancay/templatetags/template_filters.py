76cf4f6e27b0bcbc82a77be6c9a4e393518a424b
diegoe-guest <diegoe-guest@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-06-03 19:07:24 +0000

    templatetags: PEP 8, reindent, docstring
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@82 19058424-5d2e-0410-ba3e-95a0a9172157

--- amancay/templatetags/template_filters.py
+++ amancay/templatetags/template_filters.py
@@ -3,28 +3,31 @@ from django import template
 register = template.Library()
 
 def tstodate(value, arg=None):
-	from datetime import datetime
-	if not value:
-		return ''
-	try:
-		value=float(value)
-	except:
-		return ''
-	if arg is None:
-		d = datetime.fromtimestamp(value)
-		now = datetime.now()
-		if (d.year != now.year):
-			# marga says:
-			return d.strftime("%d/%m/%y")
-		elif (d.month != now.month or d.day != now.day):
-			return d.strftime("%b %e")
-		#elif (d.month != now.month):
-		#	return d.strftime("%b %e")
-		#elif (d.day != now.day):
-		#	return d.strftime("%a %e")
-		else:
-			return d.strftime("%k:%M")
-	
-	return datetime.fromtimestamp(value).strftime(arg)
+    """
+    Convert a timestamp into a human readable date.
+    """
+    from datetime import datetime
+    if not value:
+        return ''
+    try:
+        value=float(value)
+    except:
+        return ''
+    if arg is None:
+        d = datetime.fromtimestamp(value)
+        now = datetime.now()
+        if (d.year != now.year):
+            # marga says:
+            return d.strftime("%d/%m/%y")
+        elif (d.month != now.month or d.day != now.day):
+            return d.strftime("%b %e")
+        #elif (d.month != now.month):
+        #    return d.strftime("%b %e")
+        #elif (d.day != now.day):
+        #    return d.strftime("%a %e")
+        else:
+            return d.strftime("%k:%M")
+    
+    return datetime.fromtimestamp(value).strftime(arg)
 
 register.filter(tstodate)
