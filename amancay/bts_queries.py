15e3ab279bbbd23cd7e1474181bcfe2d7ecefb3d
diegoe-guest <diegoe-guest@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-06-03 19:01:02 +0000

    bts_queries: reindent and pythonify process_results
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@69 19058424-5d2e-0410-ba3e-95a0a9172157

--- amancay/bts_queries.py
+++ amancay/bts_queries.py
@@ -42,52 +42,55 @@ import SOAPpy
 from sets import Set
 
 class soap_queries(bts_queries):
-	def __init__(self):
-		self.url = 'http://bugs.debian.org/cgi-bin/soap.cgi'
-		#self.url = 'http://bugs.donarmstrong.com/cgi-bin/soap.cgi'
-		self.ns = 'Debbugs/SOAP'
-		self.server = SOAPpy.SOAPProxy(self.url, self.ns)
+    '''
+    SOAP based bts_queries class.
+    '''
+    def __init__(self):
+        self.url = 'http://bugs.debian.org/cgi-bin/soap.cgi'
+        #self.url = 'http://bugs.donarmstrong.com/cgi-bin/soap.cgi'
+        self.ns = 'Debbugs/SOAP'
+        self.server = SOAPpy.SOAPProxy(self.url, self.ns)
 
-	def process_result(self, result):
-		if type(result.item) == type([]):
-			return [ item.value for item in result.item ]
-		else:
-			return [ result.item.value ]
+    def __process_result(self, result):
+        if isinstance(result.item, list):
+            return [item.value for item in result.item]
+        else:
+            return [result.item.value]
 
-	def get_bugs_status(self, bug_numbers):
-		result = self.server.get_status(bug_numbers)
-		return self.__process_result(result)
+    def get_bugs_status(self, bug_numbers):
+        result = self.server.get_status(bug_numbers)
+        return self.__process_result(result)
 
-	def get_packages_bugs(self, packages):
-		result = self.server.get_bugs("package",packages)
-		return result
+    def get_packages_bugs(self, packages):
+        result = self.server.get_bugs("package",packages)
+        return result
 
-	def get_submitters_bugs(self, emails):
-		result = self.server.get_bugs("submitter", emails)
-		return result
+    def get_submitters_bugs(self, emails):
+        result = self.server.get_bugs("submitter", emails)
+        return result
 
-	def get_maintainers_bugs(self, emails):
-		result = self.server.get_bugs("maint",emails)
-		return result
+    def get_maintainers_bugs(self, emails):
+        result = self.server.get_bugs("maint",emails)
+        return result
 
-	def get_all_packages_bugs(self, packages):
-		pkg = self.server.get_bugs("package",packages)
-		src = self.server.get_bugs("src",packages)
-		# Unite this, and return the union.
-		result = Set(pkg)
-		result.update(src)
-		return list(result)
+    def get_all_packages_bugs(self, packages):
+        pkg = self.server.get_bugs("package",packages)
+        src = self.server.get_bugs("src",packages)
+        # Unite this, and return the union.
+        result = Set(pkg)
+        result.update(src)
+        return list(result)
 
-	def get_bug_log(self, bug):
-		result = self.server.get_bug_log(bug)
-		return result
+    def get_bug_log(self, bug):
+        result = self.server.get_bug_log(bug)
+        return result
 
-	def get_tagged_bugs(self, users):
-		# TODO: ask Don to allow many users at the same time
-		result = {}
-		for user in users:
-			result[user] = self.server.get_usertag(user)
-		return result
+    def get_tagged_bugs(self, users):
+        # TODO: ask Don to allow many users at the same time
+        result = {}
+        for user in users:
+            result[user] = self.server.get_usertag(user)
+        return result
 
 # ************************ LDAP Queries *****************************
 
@@ -96,7 +99,7 @@ class soap_queries(bts_queries):
 # ************************ Sorting Functions ************************
 
 class bug_sort:
-	def cmp_log_modified (x, y):
-		return cmp(x["log_modified"], y["log_modified"])
+    def cmp_log_modified (x, y):
+        return cmp(x["log_modified"], y["log_modified"])
 
-	cmp_log_modified = staticmethod(cmp_log_modified)
+    cmp_log_modified = staticmethod(cmp_log_modified)
