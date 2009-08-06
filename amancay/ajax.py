93328d9770a3ad9d60ca96712ebf01f9da4bb7bc
diegoe-guest <diegoe-guest@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-08-06 23:00:07 +0000

    starring: implement package removal
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@131 19058424-5d2e-0410-ba3e-95a0a9172157

--- amancay/ajax.py
+++ amancay/ajax.py
@@ -12,11 +12,29 @@ def add_package(request):
 		packages = user.package_set.filter(package_name=package_name)
 		if not packages:
 			user.package_set.create(package_name=package_name)
+			return HttpResponse(status=200)
 	else:
 		packages = request.session.get('packages', [])
-		for package in packages:
-			if package == package_name:
-				return 
-		request.session['packages'].append(package_name)
+		if package_name in packages:
+			return HttpResponse(status=500)
+		else:
+			request.session['packages'].append(package_name)
+			return HttpResponse(status=200)
 
-	return HttpResponse(status=200)
+def remove_package(request):
+	"""
+	Remove a package from the watched list.
+	"""
+	user = request.user
+	package_name = request.GET['id']
+
+	if user.is_authenticated():
+		packages = user.package_set.filter(package_name=package_name)[:1]
+		if packages:
+			packages[0].delete()
+			return HttpResponse(status=200)
+	else:
+		packages = request.session.get('packages', [])
+		if package_name in packages:
+			request.session['packages'].remove(package_name)
+			return HttpResponse(status=500)
