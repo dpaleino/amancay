6aee144258634bf6e9fde85d3f777fb4c7bccfba
diegoe-guest <diegoe-guest@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-08-10 23:40:56 +0000

    urls: remove uneeded bts_webui prefix
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@138 19058424-5d2e-0410-ba3e-95a0a9172157

--- amancay/urls.py
+++ amancay/urls.py
@@ -2,38 +2,38 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 	# Normal pages
-	(r'^$', 'bts_webui.amancay.views.index',),
-	(r'^index', 'bts_webui.amancay.views.index',),
+	(r'^$', 'amancay.views.index',),
+	(r'^index', 'amancay.views.index',),
 
 	# Toolboxes
-	(r'toolbox', 'bts_webui.amancay.toolbox.render_toolbox',),
+	(r'toolbox', 'amancay.toolbox.render_toolbox',),
 
 	# Bug pages
-	(r'^package_bugs', 'bts_webui.amancay.tables.package_bugs',),
-	(r'^received_bugs', 'bts_webui.amancay.tables.received_bugs',),
-	(r'^search', 'bts_webui.amancay.search.search',),
-	(r'^selected_bugs', 'bts_webui.amancay.tables.selected_bugs',),
-	(r'^submitted_bugs', 'bts_webui.amancay.tables.submitted_bugs',),
-	(r'^tagged_bugs', 'bts_webui.amancay.tables.tagged_bugs',),
+	(r'^package_bugs', 'amancay.tables.package_bugs',),
+	(r'^received_bugs', 'amancay.tables.received_bugs',),
+	(r'^search', 'amancay.search.search',),
+	(r'^selected_bugs', 'amancay.tables.selected_bugs',),
+	(r'^submitted_bugs', 'amancay.tables.submitted_bugs',),
+	(r'^tagged_bugs', 'amancay.tables.tagged_bugs',),
 
 	# Inside pages
-	(r'^(?P<bug_number>\d+)/?$', 'bts_webui.amancay.bugs.bug',),
-	(r'^bug/(?P<bug_number>\d+)', 'bts_webui.amancay.bugs.bug',),
-	(r'^package/(?P<package_name>\w+)', 'bts_webui.amancay.views.package',),
+	(r'^(?P<bug_number>\d+)/?$', 'amancay.bugs.bug',),
+	(r'^bug/(?P<bug_number>\d+)', 'amancay.bugs.bug',),
+	(r'^package/(?P<package_name>\w+)', 'amancay.views.package',),
 
 	# Small pieces
-	(r'^ajax/package/add/', 'bts_webui.amancay.ajax.package_add',),
-	(r'^ajax/package/remove/', 'bts_webui.amancay.ajax.package_remove',),
-	(r'^ajax/bug/subscribe/', 'bts_webui.amancay.ajax.bug_subscribe',),
-	(r'^ajax/bug/unsubscribe/', 'bts_webui.amancay.ajax.bug_unsubscribe',),
-	(r'^ajax/bug/add/', 'bts_webui.amancay.ajax.bug_add',),
-	(r'^ajax/bug/remove/', 'bts_webui.amancay.ajax.bug_remove',),
+	(r'^ajax/package/add/', 'amancay.ajax.package_add',),
+	(r'^ajax/package/remove/', 'amancay.ajax.package_remove',),
+	(r'^ajax/bug/subscribe/', 'amancay.ajax.bug_subscribe',),
+	(r'^ajax/bug/unsubscribe/', 'amancay.ajax.bug_unsubscribe',),
+	(r'^ajax/bug/add/', 'amancay.ajax.bug_add',),
+	(r'^ajax/bug/remove/', 'amancay.ajax.bug_remove',),
 
 	# Account Settings
-	#   (r'^account_settings', 'bts_webui.amancay.views.account_settings',),
+	#   (r'^account_settings', 'amancay.views.account_settings',),
 
 	# Activate a pending message
-	(r'^send_message/(?P<activation_key>\w+)/$', 'bts_webui.amancay.bugs.activate_message',),
+	(r'^send_message/(?P<activation_key>\w+)/$', 'amancay.bugs.activate_message',),
 
 	# MochiKit and other static pages
 	(r'^images/(.*)$', 'django.views.static.serve',
