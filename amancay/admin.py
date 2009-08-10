4f33b77e51294ff2f11a27f4d57423f4b908bfc3
diegoe-guest <diegoe-guest@19058424-5d2e-0410-ba3e-95a0a9172157>
2009-08-10 23:40:08 +0000

    admin: enable Bug admin pages
    git-svn-id: svn+ssh://svn.debian.org/svn/bts-webui/bts_webui@134 19058424-5d2e-0410-ba3e-95a0a9172157

--- amancay/admin.py
+++ amancay/admin.py
@@ -1,6 +1,7 @@
 # vim: set sw=4 ts=4 sts=4 noet:
 
-from amancay.models import Package
+from amancay.models import Package, Bug
 from django.contrib import admin
 
 admin.site.register(Package)
+admin.site.register(Bug)
