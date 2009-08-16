ff1afd9ba1ff7475a03f15e6e412e933202a46e1
Diego Escalante Urrelo <diegoe@gnome.org>
2009-08-16 03:48:16 -0500

    registration: quick reident

--- registration/urls.py
+++ registration/urls.py
@@ -9,18 +9,18 @@ root URLConf to include this URLConf for any URL begninning with
 """
 
 from django.conf.urls.defaults import *
-#from django.views.generic.simple import direct_to_template
-#from django.contrib.auth.views import login, logout
-#from views import activate, register
 
 urlpatterns = patterns('',
-                       # Activation keys get matched by \w+ instead of the more specific
-                       # [a-fA-F0-9]+ because a bad activation key should still get to the view;
-                       # that way it can return a sensible "invalid key" message instead of a
-                       # confusing 404.
-                       (r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate'),
-                       (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
-                       (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}),
-                       (r'^register/$', 'registration.views.register'),
-                       (r'^register/complete/$', 'django.views.generic.simple.direct_to_template', {'template': 'registration/registration_complete.html'}),
-                       )
+	# Activation keys get matched by \w+ instead of the more specific
+	# [a-fA-F0-9]+ because a bad activation key should still get to the view;
+	# that way it can return a sensible "invalid key" message instead of a
+	# confusing 404.
+
+	(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
+	(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}),
+
+	(r'^register/$', 'registration.views.register'),
+	(r'^register/complete/$', 'django.views.generic.simple.direct_to_template', {'template': 'registration/registration_complete.html'}),
+
+	(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate'),
+)
