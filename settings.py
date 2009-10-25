413849c0e42fd5a97777535c9d1fa96404b181b5
David Paleino <d.paleino@gmail.com>
2009-10-25 18:09:34 +0100

    Setting TZ to Europe/Rome

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-TIME_ZONE = 'America/Argentina/Buenos_Aires'
+TIME_ZONE = 'Europe/Rome'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
